97b1b198708ee2105cb603a6c6ec2467dfe4615c
kielsonzinn <kielsonzinn@gmail.com>
2024-09-19 20:32:56 -0300

    Implementa regra da extensao

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
-# acr-extension-name
+# acr-within-string
 
 Arquivo config.json
 
+- data
+    - message
+        - descrição da mensagem
+    - inverted
+        - se deve verificar o regex de forma invertida
+    - language
+        - linguagem do snipset
+    - stringToIgnore
+        - lista de regex para ignorar strings especificas
+    - stringLineRegex
+        - lista de regex para ignorar linhas inteiras
+    - regexFile
+        - lista de regex para verificar quais arquivos devem verificar
+    - regex
+        - lista de regex para verificar na string, respeitando o campo inverted
+
 ```json
 {
-
+  "data": [
+    {
+      "message": "${TEXT} - ${FILE_PATH} - ${LINE}",
+      "inverted": true,
+      "language": "java",
+      "stringToIgnore": [
+        ""
+      ],
+      "stringLineRegex": [
+        ""
+      ],
+      "regexFile": [
+      ],
+      "regex": [
+      ]
+    }
+  ]
 }
 ```
--- src/review.py
+++ src/review.py
@@ -1,25 +1,119 @@
+import os
+import re
+
 import automatic_code_review_commons as commons
 
+
+def extrair_strings_arquivo_por_linha(conteudo):
+    regex = r'([\'"])(.*?)(?<!\\)\1'
+
+    resultado = []
+
+    for numero_linha, linha in enumerate(conteudo.splitlines(), start=1):
+        matches = re.findall(regex, linha)
+
+        for match in matches:
+            string_extraida = match[1].replace('\\"', '"').replace("\\'", "'")
+            resultado.append((numero_linha, string_extraida, linha))
+
+    return resultado
+
+
+def ler_arquivos_pasta(path, changes):
+    todas_strings = []
+
+    for change in changes:
+        caminho_arquivo = os.path.join(path, change['new_path'])
+
+        try:
+            with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
+                conteudo = arquivo.read()
+                strings_arquivo = extrair_strings_arquivo_por_linha(conteudo)
+
+                for numero_linha, string, conteudo_linha in strings_arquivo:
+                    todas_strings.append((caminho_arquivo, numero_linha, string, conteudo_linha))
+
+        except Exception as e:
+            print(f"Erro ao ler o arquivo {caminho_arquivo}: {e}")
+
+    return todas_strings
+
+
+def __check_regex_list(regex_list, text, inverted=False):
+    if inverted:
+        for regex in regex_list:
+            if re.match(regex, text):
+                return False
+
+        return True
+
+    for regex in regex_list:
+        if re.match(regex, text):
+            return True
+
+    return False
+
+
+def __review_by_config(strings, path_source, config):
+    comments = []
+
+    regex_file = config['regexFile']
+    regex_string = config['regex']
+    inverted = 'inverted' in config and config['inverted']
+    comment = config['message']
+    string_to_ignore = config['stringToIgnore']
+
+    language = None
+    string_line_regex = ".*"
+
+    if 'language' in config:
+        language = config['language']
+
+    if 'stringLineRegex' in config:
+        string_line_regex = config['stringLineRegex']
+
+    for arquivo, linha, string, conteudo_linha in strings:
+        if not __check_regex_list(regex_file, arquivo):
+            continue
+
+        if not __check_regex_list(regex_string, string, inverted):
+            continue
+
+        if __check_regex_list(string_to_ignore, string):
+            continue
+
+        if not __check_regex_list(string_line_regex, conteudo_linha):
+            continue
+
+        comment_path = arquivo.replace(path_source, "")[1:]
+        comment_description = comment
+        comment_description = comment_description.replace("${TEXT}", string)
+        comment_description = comment_description.replace("${FILE_PATH}", comment_path)
+        comment_description = comment_description.replace("${LINE}", str(linha))
+
+        comments.append(commons.comment_create(
+            comment_id=commons.comment_generate_id(comment_description),
+            comment_path=comment_path,
+            comment_description=comment_description,
+            comment_snipset=True,
+            comment_end_line=linha,
+            comment_start_line=linha,
+            comment_language=language,
+        ))
+
+    return comments
+
+
 def review(config):
-    path_target = config['path_target']
+    merge = config['merge']
+    changes = merge['changes']
     path_source = config['path_source']
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  commons.comment_create(
-    #     comment_id=commons.comment_generate_id( "" ),
-    #     comment_path="",
-    #     comment_description="",
-    #     comment_snipset=True,
-    #     comment_end_line=1,
-    #     comment_start_line=1,
-    #     comment_language="",
-    # )
+
+    strings = ler_arquivos_pasta(path_source, changes)
+
+    for config in config['data']:
+        comments.extend(__review_by_config(strings, path_source, config))
 
     return comments
